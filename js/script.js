f5cd56ed9d86930ba65cca7617642282423c51f4
MarcoAntonioNobre <0001000610@senaimgaluno.com.br>
2024-08-23 17:06:29 -0300

    Nova versão do meu portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const insta = document.getElementById('instaId');
+const linkedin = document.getElementById('linkedinId');
+const github = document.getElementById('githubId');
+const whatsapp = document.getElementById('whatsappId');
+
+insta.addEventListener('click', function () {
+    window.open('https://www.instagram.com/marconobree?igsh=MTJ4dXRweTVqMHVx', '_blank');
+});
+
+linkedin.addEventListener('click', function () {
+    window.open('https://www.linkedin.com/in/marco-nobre-730a02194', '_blank');
+});
+
+github.addEventListener('click', function () {
+    window.open('https://github.com/MarcoAntonioNobre', '_blank');
+});
+
+whatsapp.addEventListener('click', function () {
+    const numero = '5533988661359';
+    const mensagem = encodeURIComponent('Olá, estou entrando em contato com você pelo link do seu portfólio!');
+    const whatsappUrl = `https://wa.me/${numero}?text=${mensagem}`;
+    window.open(whatsappUrl, '_blank');
+});
