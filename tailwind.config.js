c4a96fa2f20e061d134e2bdfc7281dac60293907
MarcoAntonioNobre <0001000610@senaimgaluno.com.br>
2025-04-18 11:47:32 -0300

    build: Corrigindo a build no deploy

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/js/**/*.vue",
+        "./resources/css/**/*.css",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
